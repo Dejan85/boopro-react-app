2988bfc3696a4492efcf96c930fef0015bc958ed
Dejan Markovic <dejan.markovic@cinnamon.agency>
2022-11-26 02:19:01 +0100

    create interface for movies data

--- src/src/frontend/movies/useGetMoviesQuery.ts
+++ src/src/frontend/movies/useGetMoviesQuery.ts
@@ -1,9 +1,32 @@
-import { useQueries, useQuery } from "@tanstack/react-query";
+import { useQueries, UseQueryResult } from "@tanstack/react-query";
 import { getMovies } from "src/api/methods";
 import { genres } from "src/json/generes.json";
 
 export enum QueryKey {
   movies = "movies",
+}
+
+interface DataI {
+  page: number;
+  results: {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+  }[];
+  //   results: Record<string, boolean | string | number[] | number>[];
+  total_pages: number;
+  total_results: number;
 }
 
 export const useGetMoviesQuery = (page: number) => {
@@ -13,11 +36,12 @@ export const useGetMoviesQuery = (page: number) => {
         queryKey: [`${name}`, id],
         queryFn: () => getMovies(id, 1),
         staleTime: Infinity,
+        select: ({ data }: { data: DataI }) => {
+          console.log("test", data);
+        },
       };
     }),
   });
-
-  console.log("test result", results);
 
   const data = {};
 
